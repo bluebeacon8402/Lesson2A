7b7dc18656f43a47f758f5a099bbf681cd499632
Chris Sorge <crsorge@icloud.com>
2020-06-19 12:46:11 -0400

    Lesson2A

---
+++ Lesson2 Solution/Lesson2/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Lesson2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+      
+            var name = Console.ReadLine();
+            Console.ReadLine($"My name is{name}");
+            var color = Console.ReadLine; ()
+            Console.ReadLine($"My favorite color is {color})");
+            var animal = Console.ReadLine; 
+            Console.ReadLine($"My favorite animal is {animal}");
+            var band = Console.ReadLine; 
+            Console.ReadLine($"My favorite band is {band}");
+        }
+    }
+}
