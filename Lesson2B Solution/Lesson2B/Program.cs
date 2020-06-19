f8a089abaea038176c58a3cbe6755e64523fae6e
Chris Sorge <crsorge@icloud.com>
2020-06-19 14:12:01 -0400

    lesson2B

---
+++ Lesson2B Solution/Lesson2B/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.ComponentModel;
+
+namespace Lesson2B
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            var name = Console.ReadLine();
+            var result = Add(10, 20);
+            
+            int addition = Add(2, 3);
+            int timesBy = Multiply(4, 5);
+            Console.WriteLine(addition);
+            Console.WriteLine(timesBy);
+        }
+
+        public static int Add(int num1, int num2)
+        {
+            int sum = num1 + num2;
+            return sum;
+        }
+
+        public static int Multiply(int num1, int num2)
+        {
+            int mult = num1 * num2;
+            return mult;
+        }
+    }
+}
