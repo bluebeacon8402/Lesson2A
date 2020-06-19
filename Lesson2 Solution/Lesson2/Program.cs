eaf3e93be50719309699329c9389a0f903cb61aa
Chris Sorge <crsorge@icloud.com>
2020-06-19 13:26:13 -0400

    corrected code

--- Lesson2 Solution/Lesson2/Program.cs
+++ Lesson2 Solution/Lesson2/Program.cs
@@ -6,15 +6,25 @@ namespace Lesson2
     {
         static void Main(string[] args)
         {
-      
+
+            Console.WriteLine("What is your name?");
             var name = Console.ReadLine();
-            Console.ReadLine($"My name is{name}");
-            var color = Console.ReadLine; ()
-            Console.ReadLine($"My favorite color is {color})");
-            var animal = Console.ReadLine; 
-            Console.ReadLine($"My favorite animal is {animal}");
-            var band = Console.ReadLine; 
-            Console.ReadLine($"My favorite band is {band}");
+            Console.WriteLine($"My name is {name}");
+            Console.WriteLine("What is your favorite color?");
+            var color = Console.ReadLine();
+            Console.WriteLine($"My favorite color is {color}");
+            Console.WriteLine("What is your favorite animal?");
+            var animal = Console.ReadLine();
+            Console.WriteLine($"My favorite animal is {animal}");
+            Console.WriteLine("What is your favorite band?");
+            var band = Console.ReadLine(); 
+            Console.WriteLine($"My favorite band is the {band}");
+            var message = MyString(name, color, animal, band);
+            Console.WriteLine(message);
+        }
+        public static string MyString(string name, string color, string animal, string band)
+        {
+            return $"Hello, My name is {name} and my favorite color is {color}. I really like {animal} and my favorite band is {band}";
         }
     }
 }
